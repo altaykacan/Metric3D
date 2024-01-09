eba6deb2bbcc07659fa52bf3913647ddb4b8f3d2
altaykacan <altaykacan@gmail.com>
2024-01-09 18:59:15 +0100

    Add support for numpy arrays

--- mono/wrapper/predictions.py
+++ mono/wrapper/predictions.py
@@ -5,12 +5,14 @@ from PIL import Image
 
 from mono.utils.do_test import get_prediction_custom, transform_test_data_scalecano
 
-def predict_depth(model, cfg, image: Image.Image, intrinsics: tuple):
+def predict_depth(model, cfg, image: Union[Image.Image, np.ndarray], intrinsics: tuple):
     """Predict depth given a Metric3D model, it's config and an input image"""
 
     normalize_scale = cfg.data_basic.depth_range[-1]
 
-    image = np.asarray(image)
+    # Ugly way to make sure we are working with np arrays
+    if isinstance(image, Image.Image):
+        image = np.asarray(image)
 
     rgb_input, cam_models_stacks, pad, label_scale_factor = transform_test_data_scalecano(image, intrinsics, cfg.data_basic)
 
